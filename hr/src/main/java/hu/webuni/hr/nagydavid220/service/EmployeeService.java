eceb556e5bb7cf42b859de10560a8be45bbbc872
Nagy Dávid <33985144+nagydavid220@users.noreply.github.com>
2021-05-07 10:09:57 +0200

    Delete hr directory

--- hr/src/main/java/hu/webuni/hr/nagydavid220/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package hu.webuni.hr.nagydavid220.service;
-
-import hu.webuni.hr.nagydavid220.Employee;
-
-public interface EmployeeService {
-	
-	public int getPayRaisePercent(Employee employee);
-
-}
